3541f3fc8d6b6cd416f0a848132d15767256e867
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-23 14:13:47 +0300

    Change getStaticProps to function declaration syntax

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -22,8 +22,7 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  try {
+export async function getStaticProps() {  try {
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/users"
     );
